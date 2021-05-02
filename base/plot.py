4d983bcc471955ec6dd3013a07acde9040121f24
mitchellshahen <mitchell.shahen@gmail.com>
2021-05-02 17:30:14 -0400

    Added Plotting Module

---
+++ base/plot.py
@@ -0,0 +1,20 @@
+"""
+Plotting Module
+
+:title: plot.py
+
+:author: Mitchell Shahen
+
+:history: 02/05/2021
+"""
+
+
+class Plotting:
+    """
+    Class object for plotting inputted data
+    """
+
+    def __init__(self):
+        """
+        Constructor class object for the Plotting class
+        """
