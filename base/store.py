b79a495fa2aa20a4813d73662f102e2d850aba8e
mitchellshahen <mitchell.shahen@gmail.com>
2021-05-02 17:30:01 -0400

    Added Data Store

---
+++ base/store.py
@@ -0,0 +1,34 @@
+"""
+Module to define methods for storing, generating, converting, and acquiring stored data.
+
+:title: store.py
+
+:author: Mitchell Shahen
+
+:history: 02/05/2021
+"""
+
+
+class Store:
+    """
+    Class object defining methods used in the acquisition, storage, and maintenance of available data.
+    """
+
+    def __init__(self):
+        """
+        Constructor class object for the Store class.
+        """
+
+    def acquire(self):
+        """
+        Method for acquiring data
+        """
+
+        return 0
+
+    def store(self):
+        """
+        Method for storing data locally
+        """
+
+        return 0
