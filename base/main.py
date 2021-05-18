3709b0207380a63ed03c2bff7972c76ac53b31d3
mitchellshahen <mitchell.shahen@gmail.com>
2021-05-18 13:51:40 -0400

    Add Universal StellarStructure and Pressure plot

--- base/main.py
+++ base/main.py
@@ -10,12 +10,15 @@ describe a star. Additionally, plots of the stellar structure variables are rend
 """
 
 import numpy
-from plot import hr_diagram, stellar_structure_plot
+from plot import hr_diagram, pressure_contributions_plot, stellar_structure_plot
 from solve_stellar import solve_structure
-from stellar_structure import L_index, M_index, rho_index, T_index
+from stellar_structure import StellarStructure, L_index, M_index, rho_index, T_index
 from store import Store
 import units
-from util import extrapolate, stellar_info
+from util import extrapolate
+
+# specify the set of Stellar Structure Equations to use as well as any modifications
+stellar_structure = StellarStructure()
 
 
 def generate_star():
@@ -25,41 +28,28 @@ def generate_star():
     """
 
     # set the central temperature
-    # T_0 = 2e7 * units.K
+    T_0 = 2e7 * units.K
     # T_0 = 8.23e6 * units.K
-    T_0 = 1.5e7 * units.K
+    # T_0 = 1.5e7 * units.K
 
     # set the initial central density guess
-    # rho_0_guess = 8.0063e4 * units.kg / (units.m ** 3)
+    rho_0_guess = 8.0063e4 * units.kg / (units.m ** 3)
     # rho_0_guess = 5.856e4 * units.kg / (units.m ** 3)
-    rho_0_guess = 1e5 * units.kg / (units.m ** 3)
+    # rho_0_guess = 1e5 * units.kg / (units.m ** 3)
 
     # set the confidence
     confidence = 0.5
 
-    # set the decision of whether or not to normalize the results
-    normalize = False
-
     # set the decision of whether or not to save the data
-    save_data = False
+    save_data = True
 
     # solve the stellar structure equations and acquire the necessary data
     lumin_error, radius_arr, state_matrix = solve_structure(
-        T_0,
+        stellar_structure,
+        T_0=T_0,
         rho_0_guess=rho_0_guess,
-        confidence=confidence,
-        normalize=normalize
+        confidence=confidence
     )
-
-    # can only acquire and useful information about the stellar structure solutions
-    # when the solution is not normalized
-    if not normalize:
-        stellar_info(
-            error=lumin_error,
-            r=radius_arr,
-            state=state_matrix,
-            precision=3
-        )
 
     # reformat the radius and add it to the state to form a single dataset for storage
     full_data = numpy.append(
@@ -70,20 +60,25 @@ def generate_star():
 
     # save the outputted datasets for radius and state
     if save_data:
-        if normalize:
-            Store().save(data=full_data, data_filename="first_stellar_data.pickle")
-        else:
-            Store().save(data=full_data, data_filename="first_stellar_data_norm.pickle")
+        Store().save(data=full_data, data_filename="stellar_data.pickle")
 
-    # plot all the necessary graphs describing the above generated star only if it has been normalized
-    if normalize:
-        stellar_structure_plot(
-            radius=full_data[0],
-            density=full_data[rho_index + 1],
-            temperature=full_data[T_index + 1],
-            mass=full_data[M_index + 1],
-            luminosity=full_data[L_index + 1]
-        )
+    # full_data = Store().get(data_filename="stellar_data.pickle")
+
+    # plot all the necessary graphs describing the above generated star
+    stellar_structure_plot(
+        radius=full_data[0],
+        density=full_data[rho_index + 1],
+        temperature=full_data[T_index + 1],
+        mass=full_data[M_index + 1],
+        luminosity=full_data[L_index + 1]
+    )
+
+    # plot the pressure and pressure contributions
+    pressure_contributions_plot(
+        radius=full_data[0],
+        density=full_data[rho_index + 1],
+        temperature=full_data[T_index + 1]
+    )
 
 
 def generate_star_sequence():
@@ -93,7 +88,7 @@ def generate_star_sequence():
     """
 
     # number of stars to generate
-    N = 10
+    N = 50
 
     # initial central temperature to survey
     T_0_i = 1e5 * units.K
@@ -109,10 +104,10 @@ def generate_star_sequence():
     # be used to predict viable central densities for all the proceeding central temperature values
     print("\nSolving Star 1")
     __, first_radius_arr, first_state_matrix = solve_structure(
-        T_0_i,
+        stellar_structure,
+        T_0=T_0_i,
         rho_0_guess=1e4 * units.kg / (units.m ** 3),
-        confidence=0.75,
-        normalize=False
+        confidence=0.75
     )
 
     # create an array to contain all the important properties from each star;
@@ -143,10 +138,10 @@ def generate_star_sequence():
 
         # use the predicted central density to generate the star with central temperature, `in_central_temp`
         __, radius_arr, state_matrix = solve_structure(
-            in_central_temp,
+            stellar_structure,
+            T_0=in_central_temp,
             rho_0_guess=rho_0_prediction,
-            confidence=0.5,
-            normalize=False
+            confidence=0.5
         )
 
         # create an array of the same shape as `important_properties` to contain the new data
@@ -163,7 +158,7 @@ def generate_star_sequence():
         important_properties = numpy.concatenate((important_properties, new_properties), axis=1)
 
     # save the stellar sequence
-    Store().save(data=important_properties, data_filename="first_stellar_sequence.pickle")
+    Store().save(data=important_properties, data_filename="stellar_sequence.pickle")
 
     # use the stellar sequence to generate a Hertzsprung-Russell diagram
     hr_diagram(
